288d0ae80f239305df2625fbaaa669339c4a7dc9
tommiedevelops <thomas.dizon25@gmail.com>
2026-03-17 11:55:01 +1100

    added glfw submodule, updated CMakeLists.txt and tested GLFW window

--- App/Main.cpp
+++ App/Main.cpp
@@ -1,22 +1,22 @@
-#include <GL/freeglut.h>
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-        glColor3f(1,0,0); glVertex2f(-0.5f,-0.5f);
-        glColor3f(0,1,0); glVertex2f( 0.5f,-0.5f);
-        glColor3f(0,0,1); glVertex2f( 0.0f, 0.5f);
-    glEnd();
-    glutSwapBuffers();
-}
+#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(800,600);
-    glutCreateWindow("OpenGL Test");
-    glutDisplayFunc(display);
-    glutMainLoop();
-    return 0;
+
+	glfwInit();
+
+	auto window{ glfwCreateWindow(500,500,"Title",NULL,NULL) };
+
+	std::cout << "Hello world!\n";
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
 }
 
