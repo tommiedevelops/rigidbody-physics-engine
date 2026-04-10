04f80f87ca8c26fb02d116d97797f85fe6792f6d
tommiedevelops <thomas.dizon25@gmail.com>
2026-04-10 12:49:50 +1000

    added: simple rigidbody class

--- core/Physics/Rigidbody.h
+++ core/Physics/Rigidbody.h
@@ -1,14 +1,31 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 namespace PhysicsEngine
 {
 	class Rigidbody
 	{
-	private:
 		float inverseMass;
-		glm::mat3 inertiaTensor;
-		glm::vec3 comPos;
+
+		// linear position of center of mass in world space
+		glm::vec3 linearPosition; 
+
+		// orientation of quaternion relative to world space bases
+		glm::quat orientation;
+
+		// linear velocity of center of mass in world space
+		glm::vec3 linearVelocity;
+
+		// angular velocity of rigidbody relative to world space axes
+		glm::vec3 angularVelocity;
+
+		// Translation * Rotation (No scale here)
+		// For going from object space to world space
+		glm::mat4 modelMatrix;
+
 	};
 }
